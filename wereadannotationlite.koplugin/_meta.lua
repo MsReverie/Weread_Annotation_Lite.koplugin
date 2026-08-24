30c117ad5a38122c2013dd6a73ecb9fc91f25f77
MsReverie <118539391+MsReverie@users.noreply.github.com>
2026-08-24 23:18:36 +0800

    Delete wereadannotationlite.koplugin directory

--- wereadannotationlite.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "wereadannotationlite",
-    fullname = _("Weread Annotation Lite"),
-    description = _([[Show Weread underlines and thoughts on local books.]]),
-}
